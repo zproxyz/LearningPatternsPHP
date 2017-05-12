2edd936b333e90e91661eee865af92b5b9ab2e6f
zproxyz <zproxyzer@gmail.com>
2017-05-13 01:03:58 +0300

    Add Singleton Pattern

---
+++ Creational Patterns/02-Singleton/singleton.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Singleton Pattern
+ */
+
+final class ProductDb
+{
+    private $db = null;
+    public static $instance = null;
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new ProductDb();
+        }
+        return self::$instance;
+
+    }
+
+    private function __construct()
+    {
+        // set $db
+    }
+
+    public function setQuery(string $sql)
+    {
+        echo "Setting Query: " . $sql . PHP_EOL;
+    }
+
+}
+
+$db1 = ProductDb::getInstance();
+$db2 = ProductDb::getInstance();
+
+$db1->setQuery('SELECT * FROM ProductTypes');
+echo $db1 === $db2;
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Learning PHP Patterns
 ## Creational Patterns
 1. Factory
+2. Singleton
